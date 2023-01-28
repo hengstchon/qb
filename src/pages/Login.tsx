159b9139e755b0cf3e70c7ed668c36ba5f6c56e9
hengstchon <hengstchon@gmail.com>
2023-01-28 01:50:17 +0100

    add Label & Input ui, add Login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,31 @@
+import { Input } from '@/ui/Input'
+import { Label } from '@/ui/Label'
+
 export const Login = () => {
-  return <div>LoginLogin</div>
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="flex w-full max-w-md flex-col">
+        <h1 className="text-center text-3xl font-semibold">Welcome</h1>
+        <div className="mt-6 mb-16 grid gap-6 rounded-md border px-16 py-8">
+          <div className="grid gap-2">
+            <Label className="text-base leading-none" htmlFor="username">
+              Username
+            </Label>
+            <Input id="username" />
+          </div>
+          <div className="grid gap-2">
+            <Label className="text-base leading-none" htmlFor="password">
+              Password
+            </Label>
+            <Input id="password" />
+          </div>
+          <div className="mx-auto mt-2">
+            <button className="rounded-md bg-blue-100 p-2 px-12">
+              Sign in
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
---
+++ src/ui/Input.tsx
@@ -0,0 +1,23 @@
+import { cn } from '@/utils'
+import * as React from 'react'
+
+interface InputProps extends React.ComponentPropsWithoutRef<'input'> {
+  className?: string
+}
+
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <input
+        className={cn(
+          'h-10 w-full rounded-md border bg-transparent py-2 px-3 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+          className
+        )}
+        {...props}
+        ref={ref}
+      />
+    )
+  }
+)
+
+Input.displayName = 'Input'
