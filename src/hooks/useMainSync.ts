bcda1bc9711db30acba12e6c4b27c0655eed07c7
hengstchon <hengstchon@gmail.com>
2024-07-08 18:31:24 +0200

    updateMainDataAtom

--- src/hooks/useMainSync.ts
+++ src/hooks/useMainSync.ts
@@ -1,4 +1,5 @@
 import { useEffect } from 'react'
+import { produce } from 'immer'
 import { atom, useAtom, useSetAtom } from 'jotai'
 import useSWRImmutable from 'swr/immutable'
 import { API } from '@/services'
@@ -21,73 +22,137 @@ import {
 
 export const mainRidAtom = atom(0)
 
-const updateMainDataAtom = atom(null, (_, set, val: SyncData) => {
-  if (val.full_update) {
-    set(torrentsAtom, val.torrents as Torrents)
-    set(trackersAtom, val.trackers as Trackers)
-    set(categoriesAtom, val.categories as Categories)
-    set(tagsAtom, val.tags as Tags)
-    set(serverStateAtom, val.server_state as ServerState)
-  } else {
-    for (const key in val) {
-      switch (key) {
-        case 'tags':
-          set(tagsAtom, (prev) => [...prev, ...val.tags!])
-          break
-        case 'tags_removed':
-          set(tagsAtom, (prev) => prev.filter((v) => !val.tags?.includes(v)))
-          break
-        case 'categories':
-          set(categoriesAtom, (prev) => ({ ...prev, ...val.categories }))
-          break
-        case 'categories_removed':
-          set(categoriesAtom, (prev) =>
-            Object.fromEntries(
-              Object.entries(prev).filter(
-                ([k]) => !val.categories_removed?.includes(k),
-              ),
-            ),
-          )
-          break
-        case 'trackers':
-          set(trackersAtom, (prev) => ({ ...prev, ...val.trackers }))
-          break
-        case 'trackers_removed':
-          set(trackersAtom, (prev) =>
-            Object.fromEntries(
-              Object.entries(prev).filter(
-                ([k]) => !val.trackers_removed?.includes(k),
-              ),
-            ),
-          )
-          break
-        case 'torrents': {
-          set(torrentsAtom, (prev) => {
-            const newTors = { ...prev }
-            for (const [hash, props] of Object.entries(val.torrents!)) {
-              if (Object.keys(newTors).includes(hash)) {
-                newTors[hash] = { ...newTors[hash], ...props }
+const updateMainDataAtom = atom(null, (_, set, newData: SyncData) => {
+  if (newData.full_update) {
+    set(torrentsAtom, newData.torrents as Torrents)
+    set(trackersAtom, newData.trackers as Trackers)
+    set(categoriesAtom, newData.categories as Categories)
+    set(tagsAtom, newData.tags as Tags)
+    set(serverStateAtom, newData.server_state as ServerState)
+    return
+  }
+
+  const updateAtom = <T>(atom: any, updater: (draft: T) => void) => {
+    set(atom, produce<T>(updater))
+  }
+
+  for (const [key, value] of Object.entries(newData)) {
+    switch (key) {
+      case 'tags':
+        // set(tagsAtom, (prev) => [...prev, ...newData.tags!])
+        // set(
+        //   tagsAtom,
+        //   produce((draft) => draft.push(...value)),
+        // )
+        updateAtom<Tags>(tagsAtom, (draft) => {
+          draft.push(...(value as string[]))
+          // return Array.from(new Set(draft))
+        })
+        break
+      case 'tags_removed':
+        // set(tagsAtom, (prev) =>
+        //   prev.filter((v) => !newData.tags?.includes(v)),
+        // )
+        set(
+          tagsAtom,
+          produce((draft) => draft.filter((v) => !value.includes(v))),
+        )
+        break
+      case 'categories':
+        // set(categoriesAtom, (prev) => ({ ...prev, ...newData.categories }))
+        set(
+          categoriesAtom,
+          produce((draft) => Object.assign(draft, value)),
+        )
+        break
+      case 'categories_removed':
+        // set(categoriesAtom, (prev) =>
+        //   Object.fromEntries(
+        //     Object.entries(prev).filter(
+        //       ([k]) => !newData.categories_removed?.includes(k),
+        //     ),
+        //   ),
+        set(
+          categoriesAtom,
+          produce((draft) => {
+            value?.forEach((key) => delete draft[key])
+          }),
+        )
+        break
+      case 'trackers':
+        // set(trackersAtom, (prev) => ({ ...prev, ...newData.trackers }))
+        set(
+          trackersAtom,
+          produce((draft) => {
+            Object.assign(draft, value)
+          }),
+        )
+        break
+      case 'trackers_removed':
+        // set(trackersAtom, (prev) =>
+        //   Object.fromEntries(
+        //     Object.entries(prev).filter(
+        //       ([k]) => !newData.trackers_removed?.includes(k),
+        //     ),
+        //   ),
+        // )
+        set(
+          trackersAtom,
+          produce((draft) => {
+            value?.forEach((key) => delete draft[key])
+          }),
+        )
+        break
+      case 'torrents': {
+        // set(torrentsAtom, (prev) => {
+        //   const newTors = { ...prev }
+        //   for (const [hash, props] of Object.entries(newData.torrents!)) {
+        //     if (Object.keys(newTors).includes(hash)) {
+        //       newTors[hash] = { ...newTors[hash], ...props }
+        //     } else {
+        //       newTors[hash] = props
+        //     }
+        //   }
+        //   return newTors
+        // })
+        set(
+          torrentsAtom,
+          produce((draft) => {
+            Object.entries(value).forEach(([hash, props]) => {
+              if (hash in draft) {
+                Object.assign(draft[hash], props)
               } else {
-                newTors[hash] = props
+                draft[hash] = props
               }
-            }
-            return newTors
-          })
-          break
-        }
-        case 'torrents_removed':
-          set(torrentsAtom, (prev) =>
-            Object.fromEntries(
-              Object.entries(prev).filter(
-                ([k]) => !val.torrents_removed?.includes(k),
-              ),
-            ),
-          )
-          break
-        case 'server_state':
-          set(serverStateAtom, (prev) => ({ ...prev, ...val.server_state }))
-          break
+            })
+          }),
+        )
+        break
       }
+      case 'torrents_removed':
+        // set(torrentsAtom, (prev) =>
+        //   Object.fromEntries(
+        //     Object.entries(prev).filter(
+        //       ([k]) => !newData.torrents_removed?.includes(k),
+        //     ),
+        //   ),
+        // )
+        set(
+          torrentsAtom,
+          produce((draft) => {
+            value?.forEach((key) => delete draft[key])
+          }),
+        )
+        break
+      case 'server_state':
+        // set(serverStateAtom, (prev) => ({ ...prev, ...newData.server_state }))
+        set(
+          serverStateAtom,
+          produce((draft) => {
+            Object.assign(draft, value)
+          }),
+        )
+        break
     }
   }
 })
